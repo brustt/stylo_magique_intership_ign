175bf2d5ce924a898689c22146ca70685f98b692
Martin Dizier <martin.dizier@ign.fr>
2024-04-22 17:22:19 +0200

    seaganychange - start fusion object proposal

--- src/magic_pen/seg_any_change/main.py
+++ src/magic_pen/seg_any_change/main.py
@@ -8,7 +8,8 @@
 - re-install segment_anything with pip install -e .
 
 """
-from typing import Any
+import itertools
+from typing import Any, Dict
 import pandas as pd
 import numpy as np 
 from segment_anything import SamPredictor, sam_model_registry, SamAutomaticMaskGenerator
@@ -50,39 +51,78 @@ class SegAnyChange:
     def bitemporal_matching(self, **params) -> Any:
 
         masks_A = generate_masks(self.mask_generator, self.img_A)
+        print(f"N masks A : {len(masks_A)}")
         img_embedding_A = embedding_from_predictor(self.mask_generator.predictor)
         self.mask_generator.predictor.reset_image()
         
         masks_B = generate_masks(self.mask_generator, self.img_B)
+        print(f"N masks B : {len(masks_A)}")
+
         img_embedding_B = embedding_from_predictor(self.mask_generator.predictor)
         self.mask_generator.predictor.reset_image()
         
         # t -> t+1
-        x_ti = [self.compute_mask_embedding(m, img_embedding_A) for m in masks_A]
-        x_t1i1 = [self.compute_mask_embedding(m, img_embedding_B) for m in masks_A]
-        chg_ci= [self.neg_cosine_sim(x, y) for x, y in zip(x_ti, x_t1i1)]
+        x_t_mA = [self.compute_mask_embedding(m, img_embedding_A) for m in masks_A]
+        x_t1_mA = [self.compute_mask_embedding(m, img_embedding_B) for m in masks_A]
+        chg_ci= [self.neg_cosine_sim(x, y) for x, y in zip(x_t_mA, x_t1_mA)]
 
         # t+1 -> t
-        x_ti = [self.compute_mask_embedding(m, img_embedding_A) for m in masks_B]
-        x_t1i1 = [self.compute_mask_embedding(m, img_embedding_B) for m in masks_B]
-        chg_ci1 = [self.neg_cosine_sim(x, y) for x, y in zip(x_ti, x_t1i1)]
+        x_t_mB = [self.compute_mask_embedding(m, img_embedding_A) for m in masks_B]
+        x_t1_mB = [self.compute_mask_embedding(m, img_embedding_B) for m in masks_B]
+        chg_ci1 = [self.neg_cosine_sim(x, y) for x, y in zip(x_t_mB, x_t1_mB)]
+
+        # proposal_matching
+        # what to do with matching and confidence score ci and ci1 ??
 
         tmp_return = {
-            "A": {"mask":masks_A,"img_embedding":img_embedding_A},
-            "B": {"mask":masks_B,"img_embedding":img_embedding_B},
-            "conf":[chg_ci, chg_ci1]
+            "A": {"global_mask":masks_A, "img_embedding":img_embedding_A},
+            "B": {"global_mask":masks_B, "img_embedding":img_embedding_B},
+            "mask_embedding":{"t->t+1":(x_t_mA, x_t1_mA), "t+1->t":(x_t_mB, x_t1_mB)},
+            "conf":{"t->t+1":chg_ci, "t+1->t":chg_ci1}
 
         }
         return tmp_return
             
     
     def compute_mask_embedding(self, mask: np.ndarray, img_embedding: np.ndarray) -> np.ndarray:
-        # remove zero values : np.nanmean(np.where(matrix!=0,matrix,np.nan),(1, 2))
-        return np.mean(img_embedding*mask.reshape(img_embedding.shape), axis=(1, 2))
+        # 256 x 64 x 64
+        mask = mask.reshape(img_embedding.shape)
+        mask = np.where(mask!=0,1,np.nan)
+        mask_embedding = np.nanmean(img_embedding*mask, axis=(1, 2))
+        mask_embedding = np.where(np.isnan(mask_embedding),0,1)
+        return mask_embedding
     
     def neg_cosine_sim(self, x1, x2): 
         assert np.linalg.norm(x1) == np.linalg.norm(x2)
         return - (x1 @ x2) / np.linalg.norm(x1)**2
+    
+
+    def proposal_matching(self, mask_A: np.ndarray, mask_B: np.ndarray) -> Dict[int, int]:
+        """Compute overlap mapping between two sets of binary masks
+
+        - Add threshold for association
+
+        Args:
+            mask_A (np.ndarray): mask from It
+            mask_B (np.ndarray): mask from It+1
+
+        Returns:
+            Dict[int, int]: Dict of mask indices which intersects (mask_A_idx : [mask_B_idx1...mask_B_idx2])
+        """
+        inter_func = lambda x,y: np.count_nonzero(np.logical_and(x, y)) > 1
+
+        overlap = {}
+        prod_mt = list(zip(np.arange(len(mask_A)), mask_A))
+        prod_mt1 = list(zip(np.arange(len(mask_B)), mask_B))
+
+        for (mt_, mt1_) in itertools.product(prod_mt, prod_mt1):
+            if inter_func(mt_[1], mt1_[1]):
+                if mt_[0] in overlap:
+                    overlap[mt_[0] ].append(mt1_[0])
+                else:
+                    overlap[mt_[0] ] = [mt1_[0]]
+        
+        return overlap
 
 
 
@@ -90,10 +130,26 @@ class SegAnyChange:
 if __name__ == "__main__":
     pair_img = load_levircd_sample(1, seed=42)
     path_label,path_A, path_B = pair_img.iloc[0]
+    sam_params = {
+        "points_per_side": 32,
+        "points_per_batch": 64,
+        "pred_iou_thresh": 0.88,
+        "stability_score_thresh": 0.95,
+        "stability_score_offset": 1.0,
+        "box_nms_thresh": 0.7,
+        "crop_n_layers": 0,
+        "crop_nms_thresh": 0.7,
+        "crop_overlap_ratio": 512 / 1500,
+        "crop_n_points_downscale_factor":1,
+        "point_grids": None,
+        "min_mask_region_area": 0,
+        "output_mode": "binary_mask",
+    }
     segany = SegAnyChange(
         img_A=load_img(path_A),
         img_B=load_img(path_B),
-        model_type="vit_b"
+        model_type="vit_b",
+        **sam_params
     )
 
     res = segany.bitemporal_matching()
--- src/magic_pen/seg_any_change/mask_generator.py
+++ src/magic_pen/seg_any_change/mask_generator.py
@@ -181,6 +181,8 @@ class SegAnyMaskGenerator(SamAutomaticMaskGenerator):
         # Iterate over image crops
         data = MaskData()
         for crop_box, layer_idx in zip(crop_boxes, layer_idxs):
+            print("== process crops ==")
+
             crop_data = self._process_crop(image, crop_box, layer_idx, orig_size)
             data.cat(crop_data)
 
@@ -220,6 +222,9 @@ class SegAnyMaskGenerator(SamAutomaticMaskGenerator):
         # Generate masks for this crop in batches
         data = MaskData()
         for (points,) in batch_iterator(self.points_per_batch, points_for_image):
+            
+            print(f"== process batch for point : {points} ==")
+
             batch_data = self._process_batch(points, cropped_im_size, crop_box, orig_size)
             data.cat(batch_data)
             del batch_data
@@ -239,5 +244,78 @@ class SegAnyMaskGenerator(SamAutomaticMaskGenerator):
         data["boxes"] = uncrop_boxes_xyxy(data["boxes"], crop_box)
         data["points"] = uncrop_points(data["points"], crop_box)
         data["crop_boxes"] = torch.tensor([crop_box for _ in range(len(data["rles"]))])
+
+        return data
+
+    def _process_batch(
+        self,
+        points: np.ndarray,
+        im_size: Tuple[int, ...],
+        crop_box: List[int],
+        orig_size: Tuple[int, ...],
+    ) -> MaskData:
+        orig_h, orig_w = orig_size
+
+        # Run model on this batch
+        transformed_points = self.predictor.transform.apply_coords(points, im_size)
+        in_points = torch.as_tensor(transformed_points, device=self.predictor.device)
+        in_labels = torch.ones(in_points.shape[0], dtype=torch.int, device=in_points.device)
+        # masks : 64 x 3 x 1024 x 1024
+        masks, iou_preds, _ = self.predictor.predict_torch(
+            in_points[:, None, :],
+            in_labels[:, None],
+            multimask_output=True,
+            return_logits=True,
+        )
+        print("------")
+        print(f"Output predict masks torch : {masks.shape}")
+
+        # Serialize predictions and store in MaskData
+        data = MaskData(
+            masks=masks.flatten(0, 1),
+            iou_preds=iou_preds.flatten(0, 1),
+            points=torch.as_tensor(points.repeat(masks.shape[1], axis=0)),
+        )
+        del masks
+            
+        print(f"""init  masks data: {len(data["masks"])}""")
+
+
+        # Filter by predicted IoU
+        if self.pred_iou_thresh > 0.0:
+            keep_mask = data["iou_preds"] > self.pred_iou_thresh
+            data.filter(keep_mask)
+          
+        print(f"""Output filter IoU masks data: {len(data["masks"])}""")
+
+
+        # Calculate stability score
+        data["stability_score"] = calculate_stability_score(
+            data["masks"], self.predictor.model.mask_threshold, self.stability_score_offset
+        )
+        if self.stability_score_thresh > 0.0:
+            keep_mask = data["stability_score"] >= self.stability_score_thresh
+            data.filter(keep_mask)
+        
+        print(f"""Output filter stability masks data: {len(data["masks"])}""")
+        print(f""" mask data : {data["masks"][0].shape if len(data["masks"]) > 0 else "empty masks"}""")
+
+        # Threshold masks and calculate boxes
+        data["masks"] = data["masks"] > self.predictor.model.mask_threshold
+        data["boxes"] = batched_mask_to_box(data["masks"])
+
+        print(f"""Output filter mask_threshold: {len(data["masks"])}""")
+
+
+        # Filter boxes that touch crop boundaries
+        keep_mask = ~is_box_near_crop_edge(data["boxes"], crop_box, [0, 0, orig_w, orig_h])
+        if not torch.all(keep_mask):
+            data.filter(keep_mask)
+        print(f"""Output filter boundary {len(data["masks"])}""")
+
+        # Compress to RLE
+        data["masks"] = uncrop_masks(data["masks"], crop_box, orig_h, orig_w)
+        data["rles"] = mask_to_rle_pytorch(data["masks"])
+        del data["masks"]
 
         return data
