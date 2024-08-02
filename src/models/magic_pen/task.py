be2b0e2d61449259c8c9e1cc08b84ea87ef7ee98
Martin Dizier <martin.dizier@ign.fr>
2024-08-02 12:16:45 +0200

    freeze unused weights

--- src/models/magic_pen/task.py
+++ src/models/magic_pen/task.py
@@ -22,13 +22,13 @@ logger.setLevel(logging.INFO)
 
 _register_layer_not_used = [
     "model.prompt_encoder.point_embeddings.2.weight",
-    "model.prompt_encoder.point_embeddings.3.weight"
+    "model.prompt_encoder.point_embeddings.3.weight",
     "model.mask_decoder.iou_prediction_head.layers.0.weight",
     "model.mask_decoder.iou_prediction_head.layers.0.bias", 
     "model.mask_decoder.iou_prediction_head.layers.1.weight",
     "model.mask_decoder.iou_prediction_head.layers.1.bias", 
     "model.mask_decoder.iou_prediction_head.layers.2.weight", 
-    "model.mask_decoder.iou_prediction_head.layers.2.bias"
+    "model.mask_decoder.iou_prediction_head.layers.2.bias",
 ]
     
 
@@ -87,33 +87,37 @@ class MagicPenModule(pl.LightningModule):
         #  - freeze layer on key layer selection / name
             - set init weights based on known distrib
         """
-        def is_trainable_layer(trainable_name: str, layer_name: str) -> bool:
+        def match_name(trainable_name: str, layer_name: str) -> bool:
             return bool(re.search(trainable_name, layer_name))
         
         if ft_mode == "probing":
             #self.model.image_encoder.requires_grad_(False)
             for param in self.model.image_encoder.parameters():
-                param.requires_grad = False
+                param.requires_grad_(False)
 
         elif ft_mode == "adapter":
             #  ImageEncoderAdapterVit has adapter layer
             for name, l in self.model.image_encoder.named_parameters():
-                if not is_trainable_layer(ft_mode, name):
-                    l.requires_grad = False
+                if not match_name(ft_mode, name):
+                    l.requires_grad_(False)
 
         # freeze layer not contributing to backpropagation
         for name, l in self.model.named_parameters():
-            if name in _register_layer_not_used:
-                l.requires_grad = False
+            if match_name("iou_prediction_head", name):
+                l.requires_grad_(False)
+            if match_name("prompt_encoder.mask_downscaling", name):
+                l.requires_grad_(False)
+            self.model.prompt_encoder.point_embeddings[2].requires_grad_(False)
+            self.model.prompt_encoder.point_embeddings[3].requires_grad_(False)
 
     def on_before_backward(self, loss):
-        # for name, param in self.named_parameters():
-            # print(f'on before backward, param name: {name}, grad status: {param.grad.shape if param.grad is not None else None}')
+        for name, param in self.named_parameters():
+            print(f'on before backward, param name: {name}, grad status: {param.grad.shape if param.grad is not None else None}, grad require: {param.requires_grad}')
             pass
 
     def on_after_backward(self):
-        # for name, param in self.named_parameters():
-            # print(f'on after backward, param name: {name}, grad status: {param.grad.shape if param.grad is not None else None}')
+        for name, param in self.named_parameters():
+            print(f'on after backward, param name: {name}, grad status: {param.grad.shape if param.grad is not None else None}, grad require: {param.requires_grad}')
             pass
 
     def forward(self, x):
